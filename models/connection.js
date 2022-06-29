0562eccb8f9d2e3b98dcbc2e10611b8955032a32
cathe <cacateles12@gmail.com>
2022-06-29 16:06:48 -0300

    lint fix

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'model_example'
+  database: 'StoreManager',
 });
 
 module.exports = connection;
