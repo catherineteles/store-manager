073432ba7d59bba295c0438a4bc17a4d3ea5f617
cathe <cacateles12@gmail.com>
2022-06-30 15:20:41 -0300

    Fixed connection imports

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'StoreManager',
+  port: 3306,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
